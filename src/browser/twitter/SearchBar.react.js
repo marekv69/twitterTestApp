6210906bfc60ff7f735979bdf9b3f38507a35e58
Marek Vasik <vasik@upcmail.cz>
2016-01-24 17:34:11 +0100

    Searching and showing tweets implemented

--- src/browser/twitter/SearchBar.react.js
+++ src/browser/twitter/SearchBar.react.js
@@ -6,50 +6,52 @@ import {Input, Button} from 'react-bootstrap';
 export default class SearchBar extends Component {
 
   static propTypes = {
-    //tweets: PropTypes.array
+    onSearchBarButtonClick : PropTypes.func,
+    isGUIDisabledDueToLoading : PropTypes.bool
   };
 
   constructor(props) {
     super(props);
     this.state = {
-      isButtonDisabled : true,
-      currentInputValue: ""
+      currentInputValue : ""
     }
-    this.handleInputValueChange = this.handleInputValueChange.bind(this);
+    this._handleInputValueChange = this._handleInputValueChange.bind(this);
+    this._onButtonClick = this._onButtonClick.bind(this);
   }
 
-  handleInputValueChange(event) {
-    if(event.target.value === "") {
-      this.setState({isButtonDisabled : true});
-    } else {
-      this.setState({isButtonDisabled : false});
-    }
-    console.log(this.state.isButtonDisabled);
+
+  _handleInputValueChange(event) {
     this.setState({currentInputValue : event.target.value})
   }
 
+  _onButtonClick()
+  {
+    var currentInputValue = this.state.currentInputValue;
+    this.props.onSearchBarButtonClick(currentInputValue);
+  }
+
   render() {
-    let input = <Input
-      className="input"
-      onChange={this.handleInputValueChange}
-      placeholder="Type twitter username"
-      standalone
-      type="text"
-      value={this.state.currentInputValue}/>;
-
-    let confirmationButton = <Button
-      disabled={this.state.isButtonDisabled}
-      bsStyle="primary"
-      standalone>
-      Show tweets
-    </Button>;
-
-
+    console.log(this.state.currentInputValue);
     return (
       <div className="search-bar">
         <form>
-          {input}
-          {confirmationButton}
+          <Input
+            className="input"
+            disabled= {this.props.isGUIDisabledDueToLoading}
+            onChange={this._handleInputValueChange}
+            placeholder="Type twitter username"
+            standalone
+            type="text"
+            value={this.state.currentInputValue}
+          />
+          <Button
+            disabled={this.state.currentInputValue === "" || this.props.isGUIDisabledDueToLoading}
+            bsStyle="primary"
+            onClick={this._onButtonClick}
+            standalone
+          >
+            { this.props.isGUIDisabledDueToLoading ? "Loading..." : "Show tweets" }
+          </Button>
         </form>
       </div>
     );
