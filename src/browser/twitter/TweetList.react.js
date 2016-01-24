73467b823941b9ec1de31a64d0cbb0d2434e1453
Marek Vasik <vasik@upcmail.cz>
2016-01-24 16:06:55 +0100

    Added GUI elements

--- src/browser/twitter/TweetList.react.js
+++ src/browser/twitter/TweetList.react.js
@@ -1,6 +1,8 @@
+import './TweetList.styl';
 import Component from 'react-pure-render/component';
 import React, {PropTypes} from 'react';
 import Tweet from 'react-tweet'
+import {Input,ButtonGroup, DropdownButton, Button, MenuItem} from 'react-bootstrap';
 
 export default class TweetList extends Component {
 
@@ -19,6 +21,31 @@ export default class TweetList extends Component {
 
     return (
       <div className="tweet-list">
+        <ButtonGroup>
+          <DropdownButton disabled title="Sort by date" bsStyle="info" bsSize="xsmall" id="dropdown1">
+            <MenuItem eventKey="1">Ascending</MenuItem>
+            <MenuItem eventKey="2">Descending</MenuItem>
+          </DropdownButton>
+          <DropdownButton disabled title="Sort by likes" bsStyle="info" bsSize="xsmall" id="dropdown2">
+            <MenuItem eventKey="1">Ascending</MenuItem>
+            <MenuItem eventKey="2">Descending</MenuItem>
+          </DropdownButton>
+          <Button
+            disabled
+            bsStyle="success"
+            bsSize="xsmall">
+            Show modal info
+          </Button>
+        </ButtonGroup>
+        <Input
+          bsSize="small"
+          disabled
+          placeholder="Search in tweets"
+          type="text"
+
+          //TODO: search in tweets
+          //onChange={this.handleChange}
+        />
         {tweets}
       </div>
     );
